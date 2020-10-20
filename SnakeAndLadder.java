6e122f6528562d835f2139270c7c63ecb5c37052
Rajat Gundi <glrajat@gmail.com>
2020-10-20 11:24:24 +0530

    Checking for possible options after die roll

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -9,11 +9,26 @@ class Player
 	{
     this.id=id;
   }
+	int option()
+	{
+		int fate=(int)((Math.random()*10)%3);
+		return fate;
+	}
 	int roll()
 	{
 		//Player rolls the die to get number between 1 & 6
 		int dieNumber=(int)((Math.random()*10)%6+1);
-		return dieNumber;
+		switch (option())
+		{
+			case 0:
+				System.out.println("No Play");
+				break;
+			case 1:
+				System.out.println("Ladder");
+				break;
+			default:
+				System.out.println("Snake")
+		}
 	}
 }
 public class SnakeAndLadder
@@ -22,6 +37,6 @@ public class SnakeAndLadder
 	{
     Player p=new Player(1);
 		System.out.println("Player position is : "+p.position);
-		System.out.println("Player rolled : "+p.roll());
+		p.roll();
 	}
 }
